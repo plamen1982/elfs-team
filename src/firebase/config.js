f8965b898c5b92824ea6fcef617467238bf7257c
plamen1982 <plamen.hristov@ymail.com>
2019-09-29 19:53:58 +0300

    First commit

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDrymTnjHgABRa11g3IK7afhdETH--qIDQ",
+    authDomain: "elfs-team.firebaseapp.com",
+    databaseURL: "https://elfs-team.firebaseio.com",
+    projectId: "elfs-team",
+    storageBucket: "",
+    messagingSenderId: "260809060231",
+    appId: "1:260809060231:web:da25741bc19be2218e2d22",
+    measurementId: "G-NFCCMLPQ30"
+  };
+  // Initialize Firebase
+export default firebaseConfig;
